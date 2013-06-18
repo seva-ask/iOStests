8ac9a2cefc881db83840bcc747747e5cbac7eb93
Vsevolod Ivanov <vi@touchin.ru>
2013-06-18 19:46:57 +0400

    new api ended

--- Hashbot_iOS/HashBot/HashBot/Data/TweetDataProvider.cs
+++ Hashbot_iOS/HashBot/HashBot/Data/TweetDataProvider.cs
@@ -18,9 +18,8 @@ namespace HashBot.Data
 
 		public void GetTweets(string hashTag, Action<IEnumerable<TweetEntry>> callback)
 		{
-			var request = new RestRequest ("https://api.twitter.com/1.1/search/tweets.json?q=" + hashTag);
-			_client.Authenticator.Authenticate (_client, request);
-	//		request.AddHeader ("Authorization", "OAuth oauth_consumer_key=\"MWKIDCTFFkowvIumacFEg\", oauth_nonce=\"724608944c79de80b984c58a76904afa\", oauth_signature=\"XlDuuNwnz%2FOGOp9tyJt%2FR2twIiE%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"" + DateTime.Now.Ticks + "\", oauth_token=\"1527662420-F5jOhT11DpZvozf3IshMaAetd1FFN41KZyf84Io\", oauth_version=\"1.0\"");
+			var request = new RestRequest ("https://api.twitter.com/1.1/search/tweets.json");
+			request.AddParameter ("q", hashTag);
 			_client.ExecuteAsync<TweetSearchResult>(request, response =>
 			{
 				callback(response.Data.Statuses);
